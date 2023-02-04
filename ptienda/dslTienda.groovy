b579b4e99edd71322aabfda0ef39c312c4f430f2
Jorge <jorgeperez793@gmail.com>
2023-02-04 22:35:14 +0100

    proyecto_ejemplo

---
+++ ptienda/dslTienda.groovy
@@ -0,0 +1,24 @@
+
+// import jenkins-auto-approve-script.groovy
+
+job('Job-pTienda') {
+    description('Job Padre para crear una tarea Hija que despliegue la App Tienda del Corte Ingles.sdasdasdasd')
+    scm {
+        git('https://github.com/jorgelp27/jenkinsPTienda.git', 'master'){ 
+            node -> 
+                node / gitConfigName('jorgelp27')
+                node / gitConfigEmail('jorgeperez793@gmail.com')
+        }
+    }
+    triggers {
+     //   scm('H/5 * * * *')
+        githubPush()
+    }
+    
+    steps {
+        shell("bash install.sh")
+    }
+}
+
+
+
